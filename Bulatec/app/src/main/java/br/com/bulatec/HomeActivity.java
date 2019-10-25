48b4172705c6269e8e18715738980cbbf50a0f7f
LucSandes <luccas.sandes@hotmail.com>
2019-10-24 22:42:53 -0300

    mapa

--- Bulatec/app/src/main/java/br/com/bulatec/HomeActivity.java
+++ Bulatec/app/src/main/java/br/com/bulatec/HomeActivity.java
@@ -6,27 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
+    TextView tvList;
     Button btnLogout;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);  btnLogout = findViewById(R.id.btnlogout);
+
+        tvList = findViewById(R.id.textViewList);
+
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 FirebaseAuth.getInstance().signOut();
                 Intent intToMain = new Intent(HomeActivity.this, MainActivity.class);
                 startActivity(intToMain);
             }
         });
+
+
+        tvList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View c) {
+                Intent i = new Intent(HomeActivity.this,ListActivity.class);
+                startActivity(i);
+            }
+        });
     }
-}
+    }
+
+
+
+
